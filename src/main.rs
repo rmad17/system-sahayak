a8194c3f0ab34cab573532ef0c5094fb4f695c71
Sourav Basu <souravbasu17@zohomail.in>
2024-02-15 09:01:30 +0530

    Moving ui to ui.rs

--- src/main.rs
+++ src/main.rs
@@ -40,20 +40,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
-    loop {
-        terminal.draw(|f| ui(f, &mut app))?;
-        if let Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press {
-                match key.code {
-                    KeyCode::Char('q') => return Ok(()),
-                    KeyCode::Enter => return select(terminal, app),
-                    KeyCode::Down | KeyCode::Char('j') => app.next(),
-                    KeyCode::Up | KeyCode::Char('k') => app.previous(),
-                    _ => {}
-                }
-            }
-        }
-    }
+    start_tui(terminal, app);
 }
 
 fn ui(f: &mut Frame, app: &mut App) {
---
+++ src/ui.rs
@@ -0,0 +1,16 @@
+pub fn start_tui<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Enter => return select(terminal, app),
+                    KeyCode::Down | KeyCode::Char('j') => app.next(),
+                    KeyCode::Up | KeyCode::Char('k') => app.previous(),
+                    _ => {}
+                }
+            }
+        }
+    }
+}
